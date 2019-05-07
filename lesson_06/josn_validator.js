0cd47b347574e528e3dc3fffa587ec8487152726
Joseph Hany <joe.hany@hotmail.com>
2019-05-06 19:54:11 -0500

    fixed reading raw data from request

--- lesson_06/josn_validator.js
+++ lesson_06/josn_validator.js
@@ -1,14 +1,19 @@
 module.exports = (req, res, next) => {
   // we recieve json on POST or PUT
   if (req.method == 'POST' || req.method === 'PUT') {
-    req.on('data', data => {
+    let data = '';
+    req.on('data', chunk => {
+      data += chunk;
+    });
+    req.on('end', () => {
       try {
         req.body = JSON.parse(data);
         return next();
       } catch(error) {
-        res.status(400).send({ error: 'invalid json'});
+        //res.status(400).send({ error: 'invalid json'});
+        return next(new Error('JSON NOT VALID'));
       }
-    });
+    })
   } else {
     return next();
   }
